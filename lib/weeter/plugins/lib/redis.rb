0bc92ae74f5fdc31f49a3436198142237ce089e4
Luke Melia <luke@lukemelia.com>
2012-12-04 17:56:08 -0500

    Add errback for redis client and allow creation of more than one redis client - Luke Melia/Stefan Penner

--- lib/weeter/plugins/lib/redis.rb
+++ lib/weeter/plugins/lib/redis.rb
@@ -5,11 +5,10 @@ module Weeter
     module Net
       module Redis
         def create_redis_client
-          @redis ||= begin
-            redis = EM::Hiredis.connect(@config.redis_uri)
-            redis.callback { Weeter.logger.info "Connected to Redis" }
-            redis
-          end
+          redis = EM::Hiredis.connect(@config.redis_uri)
+          redis.callback { Weeter.logger.info "Connected to Redis" }
+          redis.errback { |message| Weeter.logger.err "Failed to connect to Redis: #{message}" }
+          redis
         end
       end
     end
