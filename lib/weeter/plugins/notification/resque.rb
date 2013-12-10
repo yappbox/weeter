2a8a19eee6d576bc6d666dda0dbcf87a45a80ddd
Stefan Penner <stefan.penner@gmail.com>
2013-12-10 14:31:03 -0500

    add a uniq job id

--- lib/weeter/plugins/notification/resque.rb
+++ lib/weeter/plugins/notification/resque.rb
@@ -9,19 +9,19 @@ module Weeter
         end
 
         def publish_tweet(tweet_item)
-          resque_job = %Q|{"class":"WeeterPublishTweetJob","args":[#{tweet_item.to_json}]}|
+          resque_job = %Q|{"class":"WeeterPublishTweetJob","args":[#{tweet_item.to_json}],"jid": "#{SecureRandom.hex(12)}"}|
           Weeter.logger.info("Publishing tweet #{tweet_item['id']} from user #{tweet_item['user']['id_str']}: #{tweet_item['text']}")
           enqueue(resque_job)
         end
 
         def delete_tweet(tweet_item)
-          resque_job = %Q|{"class":"WeeterDeleteTweetJob","args":[#{tweet_item.to_json}]}|
+          resque_job = %Q|{"class":"WeeterDeleteTweetJob","args":[#{tweet_item.to_json}],"jid": "#{SecureRandom.hex(12)}"}|
           Weeter.logger.info("Deleting tweet #{tweet_item['id']} for user #{tweet_item['user']['id_str']}")
           enqueue(resque_job)
         end
 
         def notify_missed_tweets(tweet_item)
-          resque_job = %Q|{"class":"WeeterMissedTweetsJob","args":[#{tweet_item.to_json}]}|
+          resque_job = %Q|{"class":"WeeterMissedTweetsJob","args":[#{tweet_item.to_json}],"jid": "#{SecureRandom.hex(12)}"}|
           Weeter.logger.info("Notifying of missed tweets (#{tweet_item.missed_tweets_count}).")
           enqueue(resque_job)
         end
@@ -29,7 +29,7 @@ module Weeter
         def notify_rate_limiting_initiated(tweet_item, limited_keys)
           payload = tweet_item.to_hash.merge(:limited_keys => limited_keys)
           payload_json = MultiJson.encode(payload)
-          resque_job = %Q|{"class":"WeeterRateLimitingInitiatedJob","args":[#{payload_json}]}|
+          resque_job = %Q|{"class":"WeeterRateLimitingInitiatedJob","args":[#{payload_json}],"jid": "#{SecureRandom.hex(12)}"}|
           Weeter.logger.info("Initiated rate limiting with tweet: #{payload_json}")
           enqueue(resque_job)
         end
